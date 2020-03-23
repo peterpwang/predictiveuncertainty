88e2b23ec0ec0d50300dba5da8954086155883dc
pw90 <peter.p.wang@outlook.com>
2020-03-23 23:23:27 +1300

    Added correct and incorrect NLL

--- classes/abstractmodel.py
+++ classes/abstractmodel.py
@@ -6,6 +6,7 @@ from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.optimizers import SGD
 import tensorflow_probability as tfp
+import tensorflow.keras.backend as K
 
 # Helper libraries
 import numpy as np
@@ -46,43 +47,59 @@ class AbstractImageClassificationModel(ABC):
         opt = SGD(lr=0.001, momentum=0.9)
         model.compile(optimizer=opt, 
             loss='categorical_crossentropy', 
-            metrics=['accuracy', ece])
+            #metrics=['accuracy', ece, tf.keras.metrics.CategoricalCrossentropy(), correct_nll])
+            metrics=['accuracy', ece, correct_nll])
 
     # Display test result
     def display_results(self, history):
         acc = history.history['accuracy']
         val_acc = history.history['val_accuracy']
-
-        loss=history.history['loss']
-        val_loss=history.history['val_loss']
-
-        ece=history.history['ece']
-        val_ece=history.history['val_ece']
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+        ece = history.history['ece']
+        val_ece = history.history['val_ece']
+        #categorical_crossentropy = history.history['categorical_crossentropy']
+        #val_categorical_crossentropy = history.history['val_categorical_crossentropy']
+        correct_nll = history.history['correct_nll']
+        val_correct_nll = history.history['val_correct_nll']
+        incorrect_nll = history.history['incorrect_nll']
+        val_incorrect_nll = history.history['val_incorrect_nll']
 
         epochs_range = range(self.epochs)
 
-        plt.figure(figsize=(8, 8))
-        plt.subplot(3, 1, 1)
+        plt.figure(figsize=(12, 12))
+        plt.subplot(2, 2, 1)
         plt.plot(epochs_range, acc, label='Training Accuracy')
         plt.plot(epochs_range, val_acc, label='Validation Accuracy')
         plt.legend(loc='lower right')
         plt.title('Training and Validation Accuracy')
 
-        plt.subplot(3, 1, 2)
+        plt.subplot(2, 2, 2)
         plt.plot(epochs_range, loss, label='Training Loss')
         plt.plot(epochs_range, val_loss, label='Validation Loss')
         plt.legend(loc='upper right')
         plt.title('Training and Validation Loss')
 
-        plt.subplot(3, 1, 3)
+        plt.subplot(2, 2, 3)
         plt.plot(epochs_range, ece, label='Training ECE')
         plt.plot(epochs_range, val_ece, label='Validation ECE')
         plt.legend(loc='upper right')
         plt.title('Training and Validation ECE')
 
+        plt.subplot(2, 2, 4)
+        plt.plot(epochs_range, correct_nll, label='Training Correct NLL')
+        plt.plot(epochs_range, val_correct_nll, label='Validation Correct NLL')
+        plt.plot(epochs_range, incorrect_nll, label='Training Incorrect NLL')
+        plt.plot(epochs_range, val_incorrect_nll, label='Validation Incorrect NLL')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation NLL')
+
         plt.savefig('result.png')
         
     def run(self):
+
+        #cfg = K.tf.ConfigProto(gpu_options={'allow_growth': True})
+        #K.set_session(K.tf.Session(config=cfg))
 
         train_images, train_labels, test_images, test_labels = self.load_dataset()
 
@@ -95,9 +112,11 @@ class AbstractImageClassificationModel(ABC):
                             batch_size=self.batch_size,
                             validation_data=(test_images, test_labels))
 
-        _, acc, ece = model.evaluate(test_images, test_labels, verbose=0)
+        _, acc, ece, correct_nll, incorrect_nll = model.evaluate(test_images, test_labels, verbose=0)
         print('acc> %.3f' % acc)
         print('ece> %.3f' % ece)
+        print('correct nll> %.3f' % correct_nll)
+        print('incorrect nll> %.3f' % incorrect_nll)
 
         self.display_results(history)
 
@@ -107,5 +126,24 @@ def ece(y_true, y_pred):
     y_true = tf.math.argmax(tf.dtypes.cast(y_true, tf.int32),1)
     logits = tf.math.log(y_pred)
     return tfp.stats.expected_calibration_error(
-        10, logits=logits, labels_true=y_true)
+        25, logits=logits, labels_true=y_true)
 
+
+# Correct NLL(loss)
+def correct_nll(y_true, y_pred):
+    index_true = tf.math.argmax(y_true, axis=1, output_type='int32')
+    index_pred = tf.math.argmax(y_pred, axis=1, output_type='int32')
+    correct = tf.cast(tf.equal(index_true, index_pred), tf.float32)
+    y_true = correct * y_true
+    y_pred = correct * y_pred
+    return tf.math.reduce_sum(tf.keras.backend.categorical_crossentropy(y_true, y_pred))
+
+
+# Incorrect NLL(loss)
+def incorrect_nll(y_true, y_pred):
+    index_true = tf.math.argmax(y_true, axis=1, output_type='int32')
+    index_pred = tf.math.argmax(y_pred, axis=1, output_type='int32')
+    incorrect = tf.cast(tf.not_equal(index_true, index_pred), tf.float32)
+    y_true = incorrect * y_true
+    y_pred = incorrect * y_pred
+    return tf.math.reduce_sum(tf.keras.backend.categorical_crossentropy(y_true, y_pred))

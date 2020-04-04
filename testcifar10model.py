037ec65a4e1f90e0c921253d04598b34b8d7f20f
pw90 <peter.p.wang@outlook.com>
2020-04-05 11:04:26 +1200

    Added args to deal with parameters.

--- testcifar10model.py
+++ testcifar10model.py
@@ -1,9 +1,20 @@
 from classes import abstractmodel, cifar10model
+import torch.multiprocessing as mp
+
+import argparse
 import sys
+import os
 
 if __name__ == "__main__":
-    epochs = 10
-    if (len(sys.argv) == 2):
-        epochs = int(sys.argv[1])
-    testmodel = cifar10model.TestCIFAR10Model(epochs, 10, 64)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', default=1, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('--num_classes', default=10, type=int, metavar='N',
+                        help='number of classes')
+    parser.add_argument('--batch_size', default=128, type=int, metavar='N',
+                        help='batch size')
+    args = parser.parse_args()
+
+    testmodel = cifar10model.TestCIFAR10Model(args)
     testmodel.run()

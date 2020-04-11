dd5ecfc524ceeff43a301de204b9e34ce5990af0
pw90 <peter.p.wang@outlook.com>
2020-04-11 16:35:24 +1200

    Added the test Resnet50 to do the experiment.

--- classes/cifar10model.py
+++ classes/cifar10model.py
@@ -16,16 +16,19 @@ class AbstractCIFAR10ImageClassificationModel(AbstractImageClassificationModel):
         train_transform = transforms.Compose([
             # These two lines help to improve accuracy but take about 240s/epoch on ResNet50.
             # Without them, one epoch only takes about 30s on ResNet50.
+            # But later test with these lines got good results (~20s). So let's put them here.
             #transforms.Resize(256),
             #transforms.CenterCrop(224),
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
         test_transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
         dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
@@ -90,3 +93,100 @@ class Resnet50CIFAR10Model(AbstractCIFAR10ImageClassificationModel):
     def define_model(self):
         return torch.hub.load('pytorch/vision:v0.4.2', 'resnet50', pretrained=False).cuda()
 
+
+# This model is from https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
+# It is used to repeat the experiment.
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def ResNet50():
+    return ResNet(Bottleneck, [3,4,6,3])
+
+class Resnet50CIFAR10ModelTest(AbstractCIFAR10ImageClassificationModel):
+    
+    # Set model
+    def define_model(self):
+        return ResNet(Bottleneck, [3,4,6,3]).cuda()
---
+++ resnet50cifar10modeltest.py
@@ -0,0 +1,24 @@
+from classes import abstractmodel, cifar10model
+import torch.multiprocessing as mp
+
+import argparse
+import sys
+import os
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', default=1, type=int, metavar='N',
+                        help='number of total epochs to run (default 1)')
+    parser.add_argument('--num_classes', default=10, type=int, metavar='N',
+                        help='number of classes (default 10)')
+    parser.add_argument('--batch_size', default=128, type=int, metavar='N',
+                        help='batch size (default 128)')
+    parser.add_argument('--lr', default=0.1, type=float, 
+                        help='learning rate (default 0.1)')
+    parser.add_argument('--resume', action='store_true', 
+                        help='resume from checkpoint')
+    args = parser.parse_args()
+
+    model = cifar10model.Resnet50CIFAR10ModelTest(args)
+    model.run()

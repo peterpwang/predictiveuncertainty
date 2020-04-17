6840677f57c7610e9afb787cd11b635b757bba1a
pw90 <peter.p.wang@outlook.com>
2020-04-17 23:42:10 +1200

    Fixed focal loss error.

--- classes/abstractfocalmodel.py
+++ classes/abstractfocalmodel.py
@@ -13,7 +13,7 @@ class AbstractFocalModel(AbstractImageClassificationModel):
     def compile_model(self, net):
         print("Learning rate is set to ", self.learning_rate)
         optimizer = optim.SGD(net.parameters(), lr=self.learning_rate, momentum=0.9, weight_decay=5e-4)
-        criterion = focal_loss().cuda(0)
+        criterion = focal_loss
         return optimizer, criterion
 
 
@@ -23,9 +23,11 @@ def focal_loss(y_pred, y):
     epsilon = 1e-7
 
     y_pred = F.softmax(y_pred, dim=1)
-    y_hot = F.one_hot(y, num_classes=self.num_classes)
+    y_hot = F.one_hot(y, num_classes=y_pred.shape[1])
     pt_plus = y_pred * y_hot
-    pt_minus = (1 - y_hot) * (1 - y_pred)
+    pt_minus = (1.0 - y_hot) * (1.0 - y_pred)
     pt = pt_plus + pt_minus
 
-    return -1 * alpah * torch.pow(1 - pt, gamma) * torch.log(epsilon + pt)
+    focal_loss =  -1.0 * alpha * torch.pow(1.0 - pt, gamma) * torch.log(epsilon + pt)
+    #print("fl:", focal_loss.sum() / y.shape[0])
+    return focal_loss.sum() / y.shape[0]

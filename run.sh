5e5927cdd347b2adb7f845d5584342d3773979a3
pw90 <peter.p.wang@outlook.com>
2020-04-22 22:56:31 +1200

    Added extra running parameters.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-python3 $1 --epochs 150 --lr=0.1
-python3 $1 --epochs 100 --lr=0.01 --resume
-python3 $1 --epochs 100 --lr=0.001 --resume
+python3 $1 --epochs 150 --lr=0.1 $2 $3 $4
+python3 $1 --epochs 100 --lr=0.01 --resume $2 $3 $4
+python3 $1 --epochs 100 --lr=0.001 --resume $2 $3 $4

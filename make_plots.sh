706efb48afb5abc37cd9ec78203f9ddff9182f20
pw90 <peter.p.wang@outlook.com>
2020-05-09 15:26:24 +1200

    Added ECE plot.

--- make_plots.sh
+++ make_plots.sh
@@ -11,3 +11,15 @@ awk -F ',' '{print $1}' results/EfficientNetB0CIFAR10Model_result.tsv > results/
 paste -d ',' results/Resnet50CIFAR10Model_NLL.csv results/Resnet50CIFAR10FocalGamma1Model_NLL.csv results/Resnet50CIFAR10FocalGamma2Model_NLL.csv results/Resnet50CIFAR10FocalGamma3Model_NLL.csv results/Densenet121CIFAR10Model_NLL.csv results/Densenet121CIFAR10FocalModel_NLL.csv results/EfficientNetB0CIFAR10Model_NLL.csv > results/NLL.csv
 python3 make_plot.py results/NLL.csv NLL
 rm results/Resnet50CIFAR10Model_NLL.csv results/Resnet50CIFAR10FocalGamma1Model_NLL.csv results/Resnet50CIFAR10FocalGamma2Model_NLL.csv results/Resnet50CIFAR10FocalGamma3Model_NLL.csv results/Densenet121CIFAR10Model_NLL.csv results/Densenet121CIFAR10FocalModel_NLL.csv results/EfficientNetB0CIFAR10Model_NLL.csv results/NLL.csv
+
+# Make ECE plot
+awk -F ',' '{print $12}' results/Resnet50CIFAR10Model_result.tsv > results/Resnet50CIFAR10Model_ECE.csv
+awk -F ',' '{print $12}' results/Resnet50CIFAR10FocalGamma1Model_result.tsv > results/Resnet50CIFAR10FocalGamma1Model_ECE.csv
+awk -F ',' '{print $12}' results/Resnet50CIFAR10FocalGamma2Model_result.tsv > results/Resnet50CIFAR10FocalGamma2Model_ECE.csv
+awk -F ',' '{print $12}' results/Resnet50CIFAR10FocalGamma3Model_result.tsv > results/Resnet50CIFAR10FocalGamma3Model_ECE.csv
+awk -F ',' '{print $12}' results/Densenet121CIFAR10Model_result.tsv > results/Densenet121CIFAR10Model_ECE.csv
+awk -F ',' '{print $12}' results/Densenet121CIFAR10FocalModel_result.tsv > results/Densenet121CIFAR10FocalModel_ECE.csv
+awk -F ',' '{print $12}' results/EfficientNetB0CIFAR10Model_result.tsv > results/EfficientNetB0CIFAR10Model_ECE.csv
+paste -d ',' results/Resnet50CIFAR10Model_ECE.csv results/Resnet50CIFAR10FocalGamma1Model_ECE.csv results/Resnet50CIFAR10FocalGamma2Model_ECE.csv results/Resnet50CIFAR10FocalGamma3Model_ECE.csv results/Densenet121CIFAR10Model_ECE.csv results/Densenet121CIFAR10FocalModel_ECE.csv results/EfficientNetB0CIFAR10Model_ECE.csv > results/ECE.csv
+python3 make_plot.py results/ECE.csv ECE
+rm results/Resnet50CIFAR10Model_ECE.csv results/Resnet50CIFAR10FocalGamma1Model_ECE.csv results/Resnet50CIFAR10FocalGamma2Model_ECE.csv results/Resnet50CIFAR10FocalGamma3Model_ECE.csv results/Densenet121CIFAR10Model_ECE.csv results/Densenet121CIFAR10FocalModel_ECE.csv results/EfficientNetB0CIFAR10Model_ECE.csv results/ECE.csv

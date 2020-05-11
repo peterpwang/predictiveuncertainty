6e029b3b358227c36d8f373059ac08cdfe25cbb3
pw90 <peter.p.wang@outlook.com>
2020-05-11 22:36:58 +1200

    Added Resnet50 focal model on Mini Imagenet.

---
+++ classes/miniimagenetfocalmodel.py
@@ -0,0 +1,67 @@
+from .abstractfocalmodel import AbstractFocalModel
+from .util import MiniImagenetDatasetFolder, make_mini_imagenet_dataset
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.models as models
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.datasets.vision import VisionDataset
+from PIL import Image
+
+
+# Abstract Mini Imagenet model
+class AbstractMiniImagenetFocalModel(AbstractFocalModel):
+
+    # Load dataset and split into training and test sets.
+    def load_dataset(self):
+        train_transform = transforms.Compose([
+            #transforms.Resize(256),
+            #transforms.CenterCrop(224),
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
+
+        dataset = MiniImagenetDatasetFolder(root='./data/mini-imagenet/', transform=train_transform)
+        medianset, testset = torch.utils.data.random_split(dataset, [90000, 10000])
+        trainset, validationset = torch.utils.data.random_split(medianset, [80000, 10000])
+
+        trainloader = torch.utils.data.DataLoader(trainset, 
+                batch_size=128,
+                shuffle=True, 
+                num_workers=2, 
+                pin_memory=True)
+        validationloader = torch.utils.data.DataLoader(validationset, 
+                batch_size=128,
+                shuffle=True, 
+                num_workers=2, 
+                pin_memory=True)
+        testloader = torch.utils.data.DataLoader(testset, 
+                batch_size=128,
+                shuffle=False, 
+                num_workers=2, 
+                pin_memory=True)
+        #print(len(trainloader), len(validationloader), len(testloader))
+
+        return trainloader, validationloader, testloader
+
+
+# Resnet50 Mini Imagenet model
+class Resnet50MiniImagenetFocalModel(AbstractMiniImagenetFocalModel):
+    
+    # Set model
+    def define_model(self):
+        return torch.hub.load('pytorch/vision:v0.4.2', 'resnet50', pretrained=False).cuda()
+
+
+# Densenet121 Mini Imagenet model
+class Densenet121MiniImagenetFocalModel(AbstractMiniImagenetFocalModel):
+    
+    # Set model
+    def define_model(self):
+        return torch.hub.load('pytorch/vision:v0.4.2', 'densenet121', pretrained=False).cuda()
+
+

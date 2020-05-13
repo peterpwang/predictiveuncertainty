3afde5c97011f1cce2aafd83ebc22d2844284b12
pw90 <peter.p.wang@outlook.com>
2020-05-13 21:07:00 +1200

    Added recording running time.

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
-python3 $1 --epochs 150 --lr=0.1 $2 $3 $4
-python3 $1 --epochs 100 --lr=0.01 --resume $2 $3 $4
-python3 $1 --epochs 100 --lr=0.001 --resume $2 $3 $4
+START_TIME=$SECONDS
+python3 $1 --epochs 1 --lr=0.1 $2 $3 $4
+#python3 $1 --epochs 100 --lr=0.01 --resume $2 $3 $4
+#python3 $1 --epochs 100 --lr=0.001 --resume $2 $3 $4
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo $1 " took " $ELAPSED_TIME >> run_log.txt
+

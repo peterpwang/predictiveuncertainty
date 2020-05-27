0e0e9ac1ae64a65f2cd34eb0020c458715d0a75e
Pei <peter.p.wang@outlook.com>
2020-05-27 20:47:42 +1200

    Removed useless TensorFlow files.

--- obsolete/vgg2cifar10model.py
+++
@@ -1,9 +0,0 @@
-from classes import abstractmodel, cifar10model
-import sys
-
-if __name__ == "__main__":
-    epochs = 10
-    if (len(sys.argv) == 2):
-        epochs = int(sys.argv[1])
-    vgg2model = cifar10model.VGG2CIFAR10Model(epochs, 10, 64)
-    vgg2model.run()
--- obsolete/vgg3dropoutdataaugmentationcifar10model.py
+++
@@ -1,9 +0,0 @@
-from classes import abstractmodel, cifar10model
-import sys
-
-if __name__ == "__main__":
-    epochs = 10
-    if (len(sys.argv) == 2):
-        epochs = int(sys.argv[1])
-    vgg3ddamodel = cifar10model.VGG3DropoutDataAugmentationCIFAR10Model(epochs, 10, 64)
-    vgg3ddamodel.run()

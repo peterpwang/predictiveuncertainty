6ecc2325849d2958631eedebb90c9a3024870987
pw90 <peter.p.wang@outlook.com>
2020-05-30 11:36:21 +1200

    Fixed number of classes error

--- poolcnntwentynewsgroupsfocalmodel.py
+++ poolcnntwentynewsgroupsfocalmodel.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', default=1, type=int, metavar='N',
                         help='number of total epochs to run (default 1)')
-    parser.add_argument('--num_classes', default=10, type=int, metavar='N',
-                        help='number of classes (default 10)')
+    parser.add_argument('--num_classes', default=20, type=int, metavar='N',
+                        help='number of classes (default 20)')
     parser.add_argument('--batch_size', default=128, type=int, metavar='N',
                         help='batch size (default 128)')
     parser.add_argument('--lr', default=0.1, type=float, 

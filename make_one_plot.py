b74677c2cc7cd9ef158e261b2a3a08c384695afb
Pei <peter.p.wang@outlook.com>
2020-06-25 22:00:29 +1200

    Merged make_one_plot and make_plot

--- make_one_plot.py
+++ make_one_plot.py
@@ -7,12 +7,36 @@ if __name__ == "__main__":
     title = sys.argv[2]
     legend_position = sys.argv[3]
     rolling = int(sys.argv[4])
-    x_label = sys.argv[5];
-    y_label = sys.argv[6];
+    y1_label = sys.argv[5];
+    y2_label = sys.argv[6];
+    y3_label = ''
+    y4_label = ''
+    y5_label = ''
+    y6_label = ''
+    y7_label = ''
+    y8_label = ''
+    if (len(sys.argv)>=7):
+        y3_label = sys.argv[7];
+    if (len(sys.argv)>=8):
+        y4_label = sys.argv[8];
+    if (len(sys.argv)>=9):
+        y5_label = sys.argv[9];
+    if (len(sys.argv)>=10):
+        y6_label = sys.argv[10];
+    if (len(sys.argv)>=11):
+        y7_label = sys.argv[11];
+    if (len(sys.argv)>=12):
+        y8_label = sys.argv[12];
 
     x=[]
     y1=[]
     y2=[]
+    y3=[]
+    y4=[]
+    y5=[]
+    y6=[]
+    y7=[]
+    y8=[]
 
     # Open file
     with open(cvsfile_name, 'r') as csvfile:
@@ -25,28 +49,82 @@ if __name__ == "__main__":
             x.append(i)
             y1.append(float(row[0]))
             y2.append(float(row[1]))
+            if(y3_label != ''):
+                y3.append(float(row[2]))
+            if(y4_label != ''):
+                y4.append(float(row[3]))
+            if(y5_label != ''):
+                y5.append(float(row[4]))
+            if(y6_label != ''):
+                y6.append(float(row[5]))
+            if(y7_label != ''):
+                y7.append(float(row[6]))
+            if(y8_label != ''):
+                y8.append(float(row[7]))
             i += 1
 
     # Calculate moving average 
     xx=[]
     yy1=[]
     yy2=[]
+    yy3=[]
+    yy4=[]
+    yy5=[]
+    yy6=[]
+    yy7=[]
+    yy8=[]
 
     i = 0
     for row in range(len(x)-rolling+1):
         xx.append(i)
         s1 = 0.0
         s2 = 0.0
+        s3 = 0.0
+        s4 = 0.0
+        s5 = 0.0
+        s6 = 0.0
+        s7 = 0.0
+        s8 = 0.0
         for j in range(rolling):
             s1 += y1[row+j]
             s2 += y2[row+j]
+            if(y3_label != ''):
+                s3 += y3[row+j]
+            if(y4_label != ''):
+                s4 += y4[row+j]
+            if(y5_label != ''):
+                s5 += y5[row+j]
+            if(y6_label != ''):
+                s6 += y6[row+j]
+            if(y7_label != ''):
+                s7 += y7[row+j]
+            if(y8_label != ''):
+                s8 += y8[row+j]
 
         yy1.append(s1)
         yy2.append(s2)
+        yy3.append(s3)
+        yy4.append(s4)
+        yy5.append(s5)
+        yy6.append(s6)
+        yy7.append(s7)
+        yy8.append(s8)
         i += 1
 
-    plt.plot(xx, yy1, label=x_label)
-    plt.plot(xx, yy2, label=y_label)
+    plt.plot(xx, yy1, label=y1_label)
+    plt.plot(xx, yy2, label=y2_label)
+    if (y3_label != ''):
+        plt.plot(xx, yy3, label=y3_label)
+    if (y4_label != ''):
+        plt.plot(xx, yy4, label=y4_label)
+    if (y5_label != ''):
+        plt.plot(xx, yy5, label=y5_label)
+    if (y6_label != ''):
+        plt.plot(xx, yy6, label=y6_label)
+    if (y7_label != ''):
+        plt.plot(xx, yy7, label=y7_label)
+    if (y8_label != ''):
+        plt.plot(xx, yy8, label=y8_label)
 
     plt.title(title)
     plt.legend(loc=legend_position)
--- make_plots.sh
+++ make_plots.sh
@@ -10,7 +10,7 @@ awk -F ',' '{print $1}' results/EfficientNetB0CIFAR10FocalModel_result.tsv > res
 awk -F ',' '{print $1}' results/EfficientNetB7CIFAR10Model_result.tsv > results/EfficientNetB7CIFAR10Model_NLL.csv
 awk -F ',' '{print $1}' results/EfficientNetB7CIFAR10FocalModel_result.tsv > results/EfficientNetB7CIFAR10FocalModel_NLL.csv
 paste -d ',' results/Resnet50CIFAR10Model_NLL.csv results/Resnet50CIFAR10FocalGamma1Model_NLL.csv results/Densenet121CIFAR10Model_NLL.csv results/Densenet121CIFAR10FocalModel_NLL.csv results/EfficientNetB0CIFAR10Model_NLL.csv results/EfficientNetB0CIFAR10FocalModel_NLL.csv results/EfficientNetB7CIFAR10Model_NLL.csv results/EfficientNetB7CIFAR10FocalModel_NLL.csv > results/NLL.csv
-python3 make_plot.py results/NLL.csv NLL_CIFAR10 "upper right" 5
+python3 make_one_plot.py results/NLL.csv NLL_CIFAR10 "upper right" 5 "Resnet 50" "Resnet 50(FL γ=1)" "Densenet 121" "Densenet 121(FL γ=1)" "EfficientNet B0" "EfficientNet B0(FL γ=1)" "EfficientNet B7" "EfficientNet B7(FL γ=1)" 
 rm results/Resnet50CIFAR10Model_NLL.csv results/Resnet50CIFAR10FocalGamma1Model_NLL.csv results/Densenet121CIFAR10Model_NLL.csv results/Densenet121CIFAR10FocalModel_NLL.csv results/EfficientNetB0CIFAR10Model_NLL.csv results/EfficientNetB0CIFAR10FocalModel_NLL.csv results/EfficientNetB7CIFAR10Model_NLL.csv results/EfficientNetB7CIFAR10FocalModel_NLL.csv results/NLL.csv
 
 # Make ECE plot of CIFAR10
@@ -23,7 +23,7 @@ awk -F ',' '{print $13}' results/EfficientNetB0CIFAR10FocalModel_result.tsv > re
 awk -F ',' '{print $13}' results/EfficientNetB7CIFAR10Model_result.tsv > results/EfficientNetB7CIFAR10Model_ECE.csv
 awk -F ',' '{print $13}' results/EfficientNetB7CIFAR10FocalModel_result.tsv > results/EfficientNetB7CIFAR10FocalModel_ECE.csv
 paste -d ',' results/Resnet50CIFAR10Model_ECE.csv results/Resnet50CIFAR10FocalGamma1Model_ECE.csv results/Densenet121CIFAR10Model_ECE.csv results/Densenet121CIFAR10FocalModel_ECE.csv results/EfficientNetB0CIFAR10Model_ECE.csv results/EfficientNetB0CIFAR10FocalModel_ECE.csv results/EfficientNetB7CIFAR10Model_ECE.csv results/EfficientNetB7CIFAR10FocalModel_ECE.csv > results/ECE.csv
-python3 make_plot.py results/ECE.csv ECE_CIFAR10 "best" 5
+python3 make_one_plot.py results/ECE.csv ECE_CIFAR10 "best" 5 "Resnet 50" "Resnet 50(FL γ=1)" "Densenet 121" "Densenet 121(FL γ=1)" "EfficientNet B0" "EfficientNet B0(FL γ=1)" "EfficientNet B7" "EfficientNet B7(FL γ=1)"
 rm results/Resnet50CIFAR10Model_ECE.csv results/Resnet50CIFAR10FocalGamma1Model_ECE.csv results/Densenet121CIFAR10Model_ECE.csv results/Densenet121CIFAR10FocalModel_ECE.csv results/EfficientNetB0CIFAR10Model_ECE.csv results/EfficientNetB0CIFAR10FocalModel_ECE.csv results/EfficientNetB7CIFAR10Model_ECE.csv results/EfficientNetB7CIFAR10FocalModel_ECE.csv results/ECE.csv
 
 # Make Test error plot of CIFAR10
@@ -36,7 +36,7 @@ awk -F ',' '{print (1-$15)}' results/EfficientNetB0CIFAR10FocalModel_result.tsv 
 awk -F ',' '{print (1-$15)}' results/EfficientNetB7CIFAR10Model_result.tsv > results/EfficientNetB7CIFAR10Model_Error.csv
 awk -F ',' '{print (1-$15)}' results/EfficientNetB7CIFAR10FocalModel_result.tsv > results/EfficientNetB7CIFAR10FocalModel_Error.csv
 paste -d ',' results/Resnet50CIFAR10Model_Error.csv results/Resnet50CIFAR10FocalGamma1Model_Error.csv results/Densenet121CIFAR10Model_Error.csv results/Densenet121CIFAR10FocalModel_Error.csv results/EfficientNetB0CIFAR10Model_Error.csv results/EfficientNetB0CIFAR10FocalModel_Error.csv results/EfficientNetB7CIFAR10Model_Error.csv results/EfficientNetB7CIFAR10FocalModel_Error.csv > results/Error.csv
-python3 make_plot.py results/Error.csv "TestError_CIFAR10" "upper right" 5
+python3 make_one_plot.py results/Error.csv "TestError_CIFAR10" "upper right" 5 "Resnet 50" "Resnet 50(FL γ=1)" "Densenet 121" "Densenet 121(FL γ=1)" "EfficientNet B0" "EfficientNet B0(FL γ=1)" "EfficientNet B7" "EfficientNet B7(FL γ=1)"
 rm results/Resnet50CIFAR10Model_Error.csv results/Resnet50CIFAR10FocalGamma1Model_Error.csv results/Densenet121CIFAR10Model_Error.csv results/Densenet121CIFAR10FocalModel_Error.csv results/EfficientNetB0CIFAR10Model_Error.csv results/EfficientNetB0CIFAR10FocalModel_Error.csv results/EfficientNetB7CIFAR10Model_Error.csv results/EfficientNetB7CIFAR10FocalModel_Error.csv results/Error.csv
 
 # Make NLL plot of CIFAR100
@@ -49,7 +49,7 @@ awk -F ',' '{print $1}' results/EfficientNetB0CIFAR100FocalModel_result.tsv > re
 awk -F ',' '{print $1}' results/EfficientNetB7CIFAR100Model_result.tsv > results/EfficientNetB7CIFAR100Model_NLL.csv
 awk -F ',' '{print $1}' results/EfficientNetB7CIFAR100FocalModel_result.tsv > results/EfficientNetB7CIFAR100FocalModel_NLL.csv
 paste -d ',' results/Resnet50CIFAR100Model_NLL.csv results/Resnet50CIFAR100FocalGamma1Model_NLL.csv results/Densenet121CIFAR100Model_NLL.csv results/Densenet121CIFAR100FocalModel_NLL.csv results/EfficientNetB0CIFAR100Model_NLL.csv results/EfficientNetB0CIFAR100FocalModel_NLL.csv results/EfficientNetB7CIFAR100Model_NLL.csv results/EfficientNetB7CIFAR100FocalModel_NLL.csv > results/NLL.csv
-python3 make_plot.py results/NLL.csv NLL_CIFAR100 "upper right" 5
+python3 make_one_plot.py results/NLL.csv NLL_CIFAR100 "upper right" 5 "Resnet 50" "Resnet 50(FL γ=1)" "Densenet 121" "Densenet 121(FL γ=1)" "EfficientNet B0" "EfficientNet B0(FL γ=1)" "EfficientNet B7" "EfficientNet B7(FL γ=1)"
 rm results/Resnet50CIFAR100Model_NLL.csv results/Resnet50CIFAR100FocalGamma1Model_NLL.csv results/Densenet121CIFAR100Model_NLL.csv results/Densenet121CIFAR100FocalModel_NLL.csv results/EfficientNetB0CIFAR100Model_NLL.csv results/EfficientNetB0CIFAR100FocalModel_NLL.csv results/EfficientNetB7CIFAR100Model_NLL.csv results/EfficientNetB7CIFAR100FocalModel_NLL.csv results/NLL.csv
 
 # Make ECE plot of CIFAR100
@@ -62,7 +62,7 @@ awk -F ',' '{print $13}' results/EfficientNetB0CIFAR100FocalModel_result.tsv > r
 awk -F ',' '{print $13}' results/EfficientNetB7CIFAR100Model_result.tsv > results/EfficientNetB7CIFAR100Model_ECE.csv
 awk -F ',' '{print $13}' results/EfficientNetB7CIFAR100FocalModel_result.tsv > results/EfficientNetB7CIFAR100FocalModel_ECE.csv
 paste -d ',' results/Resnet50CIFAR100Model_ECE.csv results/Resnet50CIFAR100FocalGamma1Model_ECE.csv results/Densenet121CIFAR100Model_ECE.csv results/Densenet121CIFAR100FocalModel_ECE.csv results/EfficientNetB0CIFAR100Model_ECE.csv results/EfficientNetB0CIFAR100FocalModel_ECE.csv results/EfficientNetB7CIFAR100Model_ECE.csv results/EfficientNetB7CIFAR100FocalModel_ECE.csv > results/ECE.csv
-python3 make_plot.py results/ECE.csv ECE_CIFAR100 "best" 5
+python3 make_one_plot.py results/ECE.csv ECE_CIFAR100 "best" 5 "Resnet 50" "Resnet 50(FL γ=1)" "Densenet 121" "Densenet 121(FL γ=1)" "EfficientNet B0" "EfficientNet B0(FL γ=1)" "EfficientNet B7" "EfficientNet B7(FL γ=1)"
 rm results/Resnet50CIFAR100Model_ECE.csv results/Resnet50CIFAR100FocalGamma1Model_ECE.csv results/Densenet121CIFAR100Model_ECE.csv results/Densenet121CIFAR100FocalModel_ECE.csv results/EfficientNetB0CIFAR100Model_ECE.csv results/EfficientNetB0CIFAR100FocalModel_ECE.csv results/EfficientNetB7CIFAR100Model_ECE.csv results/EfficientNetB7CIFAR100FocalModel_ECE.csv results/ECE.csv
 
 # Make Test error plot of CIFAR100
@@ -75,7 +75,7 @@ awk -F ',' '{print (1-$15)}' results/EfficientNetB0CIFAR100FocalModel_result.tsv
 awk -F ',' '{print (1-$15)}' results/EfficientNetB7CIFAR100Model_result.tsv > results/EfficientNetB7CIFAR100Model_Error.csv
 awk -F ',' '{print (1-$15)}' results/EfficientNetB7CIFAR100FocalModel_result.tsv > results/EfficientNetB7CIFAR100FocalModel_Error.csv
 paste -d ',' results/Resnet50CIFAR100Model_Error.csv results/Resnet50CIFAR100FocalGamma1Model_Error.csv results/Densenet121CIFAR100Model_Error.csv results/Densenet121CIFAR100FocalModel_Error.csv results/EfficientNetB0CIFAR100Model_Error.csv results/EfficientNetB0CIFAR100FocalModel_Error.csv results/EfficientNetB7CIFAR100Model_Error.csv results/EfficientNetB7CIFAR100FocalModel_Error.csv > results/Error.csv
-python3 make_plot.py results/Error.csv "TestError_CIFAR100" "upper right" 5
+python3 make_one_plot.py results/Error.csv "TestError_CIFAR100" "upper right" 5 "Resnet 50" "Resnet 50(FL γ=1)" "Densenet 121" "Densenet 121(FL γ=1)" "EfficientNet B0" "EfficientNet B0(FL γ=1)" "EfficientNet B7" "EfficientNet B7(FL γ=1)"
 rm results/Resnet50CIFAR100Model_Error.csv results/Resnet50CIFAR100FocalGamma1Model_Error.csv results/Densenet121CIFAR100Model_Error.csv results/Densenet121CIFAR100FocalModel_Error.csv results/EfficientNetB0CIFAR100Model_Error.csv results/EfficientNetB0CIFAR100FocalModel_Error.csv results/EfficientNetB7CIFAR100Model_Error.csv results/EfficientNetB7CIFAR100FocalModel_Error.csv results/Error.csv
 
 # Make NLL plot of MiniImagenet
@@ -88,7 +88,7 @@ awk -F ',' '{print $1}' results/EfficientNetB0MiniImagenetFocalModel_result.tsv 
 awk -F ',' '{print $1}' results/EfficientNetB7MiniImagenetModel_result.tsv > results/EfficientNetB7MiniImagenetModel_NLL.csv
 awk -F ',' '{print $1}' results/EfficientNetB7MiniImagenetFocalModel_result.tsv > results/EfficientNetB7MiniImagenetFocalModel_NLL.csv
 paste -d ',' results/Resnet50MiniImagenetModel_NLL.csv results/Resnet50MiniImagenetFocalGamma1Model_NLL.csv results/Densenet121MiniImagenetModel_NLL.csv results/Densenet121MiniImagenetFocalModel_NLL.csv results/EfficientNetB0MiniImagenetModel_NLL.csv results/EfficientNetB0MiniImagenetFocalModel_NLL.csv results/EfficientNetB7MiniImagenetModel_NLL.csv results/EfficientNetB7MiniImagenetFocalModel_NLL.csv > results/NLL.csv
-python3 make_plot.py results/NLL.csv NLL_MiniImagenet "upper right" 5
+python3 make_one_plot.py results/NLL.csv NLL_MiniImagenet "upper right" 5 "Resnet 50" "Resnet 50(FL γ=1)" "Densenet 121" "Densenet 121(FL γ=1)" "EfficientNet B0" "EfficientNet B0(FL γ=1)" "EfficientNet B7" "EfficientNet B7(FL γ=1)"
 rm results/Resnet50MiniImagenetModel_NLL.csv results/Resnet50MiniImagenetFocalGamma1Model_NLL.csv results/Densenet121MiniImagenetModel_NLL.csv results/Densenet121MiniImagenetFocalModel_NLL.csv results/EfficientNetB0MiniImagenetModel_NLL.csv results/EfficientNetB0MiniImagenetFocalModel_NLL.csv results/EfficientNetB7MiniImagenetModel_NLL.csv results/EfficientNetB7MiniImagenetFocalModel_NLL.csv results/NLL.csv
 
 # Make ECE plot of MiniImagenet
@@ -101,7 +101,7 @@ awk -F ',' '{print $13}' results/EfficientNetB0MiniImagenetFocalModel_result.tsv
 awk -F ',' '{print $13}' results/EfficientNetB7MiniImagenetModel_result.tsv > results/EfficientNetB7MiniImagenetModel_ECE.csv
 awk -F ',' '{print $13}' results/EfficientNetB7MiniImagenetFocalModel_result.tsv > results/EfficientNetB7MiniImagenetFocalModel_ECE.csv
 paste -d ',' results/Resnet50MiniImagenetModel_ECE.csv results/Resnet50MiniImagenetFocalGamma1Model_ECE.csv results/Densenet121MiniImagenetModel_ECE.csv results/Densenet121MiniImagenetFocalModel_ECE.csv results/EfficientNetB0MiniImagenetModel_ECE.csv results/EfficientNetB0MiniImagenetFocalModel_ECE.csv results/EfficientNetB7MiniImagenetModel_ECE.csv results/EfficientNetB7MiniImagenetFocalModel_ECE.csv > results/ECE.csv
-python3 make_plot.py results/ECE.csv ECE_MiniImagenet "best" 5
+python3 make_one_plot.py results/ECE.csv ECE_MiniImagenet "best" 5 "Resnet 50" "Resnet 50(FL γ=1)" "Densenet 121" "Densenet 121(FL γ=1)" "EfficientNet B0" "EfficientNet B0(FL γ=1)" "EfficientNet B7" "EfficientNet B7(FL γ=1)"
 rm results/Resnet50MiniImagenetModel_ECE.csv results/Resnet50MiniImagenetFocalGamma1Model_ECE.csv results/Densenet121MiniImagenetModel_ECE.csv results/Densenet121MiniImagenetFocalModel_ECE.csv results/EfficientNetB0MiniImagenetModel_ECE.csv results/EfficientNetB0MiniImagenetFocalModel_ECE.csv results/EfficientNetB7MiniImagenetModel_ECE.csv results/EfficientNetB7MiniImagenetFocalModel_ECE.csv results/ECE.csv
 
 # Make Test error plot of MiniImagenet
@@ -114,5 +114,5 @@ awk -F ',' '{print (1-$15)}' results/EfficientNetB0MiniImagenetFocalModel_result
 awk -F ',' '{print (1-$15)}' results/EfficientNetB7MiniImagenetModel_result.tsv > results/EfficientNetB7MiniImagenetModel_Error.csv
 awk -F ',' '{print (1-$15)}' results/EfficientNetB7MiniImagenetFocalModel_result.tsv > results/EfficientNetB7MiniImagenetFocalModel_Error.csv
 paste -d ',' results/Resnet50MiniImagenetModel_Error.csv results/Resnet50MiniImagenetFocalGamma1Model_Error.csv results/Densenet121MiniImagenetModel_Error.csv results/Densenet121MiniImagenetFocalModel_Error.csv results/EfficientNetB0MiniImagenetModel_Error.csv results/EfficientNetB0MiniImagenetFocalModel_Error.csv results/EfficientNetB7MiniImagenetModel_Error.csv results/EfficientNetB7MiniImagenetFocalModel_Error.csv > results/Error.csv
-python3 make_plot.py results/Error.csv "TestError_MiniImagenet" "upper right" 5
+python3 make_one_plot.py results/Error.csv "TestError_MiniImagenet" "upper right" 5 "Resnet 50" "Resnet 50(FL γ=1)" "Densenet 121" "Densenet 121(FL γ=1)" "EfficientNet B0" "EfficientNet B0(FL γ=1)" "EfficientNet B7" "EfficientNet B7(FL γ=1)"
 rm results/Resnet50MiniImagenetModel_Error.csv results/Resnet50MiniImagenetFocalGamma1Model_Error.csv results/Densenet121MiniImagenetModel_Error.csv results/Densenet121MiniImagenetFocalModel_Error.csv results/EfficientNetB0MiniImagenetModel_Error.csv results/EfficientNetB0MiniImagenetFocalModel_Error.csv results/EfficientNetB7MiniImagenetModel_Error.csv results/EfficientNetB7MiniImagenetFocalModel_Error.csv results/Error.csv
